b1af3118ef55013fe4f995101538904060533a95
= <bradmedeiros0@gmail.com>
2017-03-09 21:24:20 -0800

    got rid of compilers b/c i want repl

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
+
 /*
 const load_system = require('./system/system.js');
 
-const virtual_system = function(system_folder) {
-  this.system = undefined;
-  this.loaded = load_system(system_folder);
-  this.loaded.then(sys => {
-    this.system = sys;
-  });
+const virtual_system = undefined;
+const load_virtual_system = async (system_folder) => {
+  try {
+    await load_system()
+  }catch (err){
+    console.error('could not reload virtual system');
+  }
 };
 
+
+
 virtual_system.add_condition = function(name, conditionParameters) {
-  this.loaded.then(() => {
-    const conditionPath = path.resolve('./mock/conditions', name).concat('.condition.json');
-    const thePromise = new Promise((resolve,  reject) => {
-      fs.writeFile(conditionPath, JSON.stringify(conditionParameters), () => {
-        load_system('./mock').then(theSystem => {
-          sys=  theSystem;
-          resolve();
-          console.log('reloaded system');
-        });
+  const conditionPath = path.resolve('./mock/conditions', name).concat('.condition.json');
+  console.log('adding condition at ', conditionPath);
+
+  const thePromise = new Promise((resolve,  reject) => {
+    fs.writeFile(conditionPath, JSON.stringify(conditionParameters), () => {
+      load_system('./mock').then(theSystem => {
+        sys=  theSystem;
+        resolve();
+        console.log('reloaded system');
       });
-    }).catch(reject);
-    return thePromise;
-  });
-};
+    });
+  }).catch(reject);
+  return thePromise;
+}
 
 virtual_system.remove_condition = function() {
 
 };
-
-
-const addCondition = (name, conditionParameters) => {
-  console.log('adding condition at ', conditionPath);
-
-
-};
 */
 
+console.log('hello')
 
-module.exports = {
-  load_system: load,
-};
