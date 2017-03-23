f69c07584347fc77b8141837ddcc58ccd28cc2e4
= <bradmedeiros0@gmail.com>
2017-03-22 21:45:30 -0700

    added natural language method to execute actions

--- src/communication/http/routes/actions.js
+++ src/communication/http/routes/actions.js
@@ -1,3 +1,4 @@
+const natural = require('natural');
 const express = require('express');
 const router = express();
 
@@ -18,12 +19,12 @@ const create_routes = virtual_system => {
       .actions
       .filter(action => action.get_name() === req.params.action_name);
 
-    if (actions.length === 0){
-      res.status(404).jsonp({ error: "action not found" });
+    if (actions.length === 0) {
+      res.status(404).jsonp({error: "action not found"});
       return;
     }
-    else if (actions.length === 0){
-      res.status(500).jsonp({ error: 'internal server error'});
+    else if (actions.length === 0) {
+      res.status(500).jsonp({error: 'internal server error'});
       return;
     }
 
@@ -32,8 +33,8 @@ const create_routes = virtual_system => {
   });
 
   router.post('/modify/:action_name', (req, res) => {
-    if (req.body === undefined){
-      res.status(400).jsonp({ error: 'invalid parameters' });
+    if (req.body === undefined) {
+      res.status(400).jsonp({error: 'invalid parameters'});
       return;
     }
 
@@ -41,6 +42,47 @@ const create_routes = virtual_system => {
     const actionEval = req.body.actionEval;
     virtual_system.add_action(name, `(${actionEval})()`);
     res.status(200).send('ok');
+  });
+
+  router.post('/special/speech_recongition', (req, res) => {
+    const THRESHOLD = 0.7;
+
+    if (virtual_system.get_virtual_system().actions.length === 0){
+      res.jsonp({ response: 'no actions to perform'}).status(500);
+      return;
+    }
+    const action_to_perform = req.body.speech;
+    if (action_to_perform === undefined){
+      res.jsonp({ response: 'invalid parameters' }).status(500);
+    }
+
+    const distances = virtual_system
+      .get_virtual_system()
+      .actions
+      .map(action =>
+        natural.JaroWinklerDistance(
+          action.get_name(),
+          action_to_perform
+        )
+      );
+    const maxValue = distances.reduce(
+      (currentMax, currentVal, currentIndex) => {
+        if (currentVal > currentMax.max) {
+          return {
+            max: currentVal,
+            index: currentIndex,
+          }
+        } else {
+          return currentMax;
+        }
+      }, {max: distances[0], index: 0});
+
+    const action =  virtual_system.get_virtual_system().actions[maxValue.index];
+    if (maxValue.max > THRESHOLD) {
+      res.jsonp({ executed: true, action: action.get_name(), confidence: maxValue.max });
+      return;
+    }
+    res.jsonp({ executed: false, action: action.get_name(), confidence: maxValue.max });
   });
 
   return router;
