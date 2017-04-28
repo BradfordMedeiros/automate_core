1e939cacc375059500a86509db3caffab21fab90
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-04-28 01:11:25 -0700

    added sequence execute route

--- src/communication/http/routes/system/sequences.js
+++ src/communication/http/routes/system/sequences.js
@@ -15,6 +15,24 @@ const create_routes = virtual_system => {
     res.jsonp(json);
   });
 
+  router.post('/:sequence_name', (req, res) => {
+    const sequences = virtual_system.get_virtual_system()
+      .sequences
+      .filter(sequence => sequence.get_name() === req.params.sequence_name);
+
+    if (sequences.length === 0) {
+      res.status(404).jsonp({error: "action not found"});
+      return;
+    }
+    else if (sequences.length === 0) {
+      res.status(500).jsonp({error: 'internal server error'});
+      return;
+    }
+
+    const sequence = sequences[0];
+    sequence.execute().then(result => res.jsonp(result)).catch(() => res.status(500));
+  });
+
   router.post('/modify/:sequence_name', (req, res) => {
     if (req.body === undefined){
       res.status(400).jsonp({ error: 'invalid parameters' });
