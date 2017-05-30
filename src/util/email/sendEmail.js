7d89624416ba1a7a85243724e75bef3b8a129e9a
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-05-30 02:25:33 -0700

    email message improved

--- src/util/email/sendEmail.js
+++ src/util/email/sendEmail.js
@@ -1,13 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 const sendmail = require('sendmail')({silent: true});
+
+const template =  fs.readFileSync(path.join(__dirname, 'html/template.thtml')).toString();
+
+
+const processTemplate = (template, {event_name, event_message} ) => {
+  /*
+   {event_name}
+   {event_template}
+   */
+  return template.replace('{event_name}', event_name).replace('{event_message}', event_message);
+};
 
 const send_email = (email_address, message) => {
   return new Promise((resolve, reject) => {
     sendmail({
-      from: 'automate@router.com',
+      from: 'Automate@automate.com',
       to: email_address,
-      subject: 'Event Triggered',
-      html:  message,
-    }, function(err, reply) {
+      subject: 'Event- Topic: (topic), message: (message)',
+      html:  processTemplate(template, { event_name: 'Cat Door Opened Mo', event_message: 'SDoor is now open' }),
+    }, (err, reply) => {
       if (err) {
         reject(err);
       }
