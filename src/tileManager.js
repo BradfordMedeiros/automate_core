120a2fd8acf24f946f87e3453c8ca9f6e54e97bc
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-09-29 01:29:23 -0700

    more work on tilemanager

--- src/tileManager.js
+++ src/tileManager.js
@@ -1,11 +1,22 @@
 
+const fs =  require('fs');
+const path = require('path');
+
+const isDirectory = source => fs.lstatSync(source).isDirectory()
+const getDirectories = source => fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
+const tileDirectory = path.resolve('../public/tiles');
+
+const getTileName = x => path.basename(x);
+const getwebPath = x => path.relative(path.resolve('..'));
+
 const getTiles  =  () => {
   // equivalent to just getting the list of directory
   // {
   //    url: x/index.html
   //    name: (just the folder name)
   // }
-  throw (new Error('not yet implemented'));
+  //throw (new Error('not yet implemented'));
+  return getDirectories(tileDirectory);
 };
 
 const addTile = () => {
@@ -23,4 +34,5 @@ module.exports = {
   getTiles,
   addTile,
   deleteTile,
+  getDirectories,
 };
