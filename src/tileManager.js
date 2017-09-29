00c356f8123b4fe349a5fc6b7e0820271f1329a0
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-09-29 00:47:26 -0700

    mocked out tilemanager

---
+++ src/tileManager.js
@@ -0,0 +1,26 @@
+
+const getTiles  =  () => {
+  // equivalent to just getting the list of directory
+  // {
+  //    url: x/index.html
+  //    name: (just the folder name)
+  // }
+  throw (new Error('not yet implemented'));
+};
+
+const addTile = () => {
+  // check if the folder exists
+  // then unzip the folder
+  throw (new Error('not yet implemented'));
+};
+
+const deleteTile = () => {
+  // delete the folder
+  throw (new Error('not yet implemented'));
+};
+
+module.exports = {
+  getTiles,
+  addTile,
+  deleteTile,
+};
