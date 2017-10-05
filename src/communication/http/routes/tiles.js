fd97da82301ea8f550ae432e189bf6993cd22221
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-10-05 00:26:36 -0700

    added tile upload route, need to unzip

--- src/communication/http/routes/tiles.js
+++ src/communication/http/routes/tiles.js
@@ -1,5 +1,8 @@
 
 const express = require('express');
+const path = require('path');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const create_routes = tileManager  => {
   if (tileManager === undefined){
@@ -8,16 +11,40 @@ const create_routes = tileManager  => {
 
   const router = express();
 
-  b = tileManager;
   router.get('/', (req, res) => {
     // get a listing of tilenames
     const tiles = tileManager.getTiles();
     res.status(200).jsonp(tiles);
   });
 
-  /*router.post('/:tilename', (req, res) => {
+  router.post('/:tilename', (req, res) => {
     // save a tile
-  });*/
+    const tileName = req.params.tilename;
+    const form = new formidable.IncomingForm();
+
+    form.uploadDir = path.resolve(`./public/tiles/${tileName}`);
+
+    form.parse(req, (err, fields, files) => {
+      if (err){
+        res.status(400).jsonp({ error: 'internal server error' });
+      }else{
+        const fileName = Object.keys(files)[0];
+        const oldpath = files[fileName].path;
+        const newpath = path.resolve(`./public/tiles/${tileName}/index.html`);
+
+        console.log('old path: ', oldpath);
+        console.log('new path: ', newpath);
+
+        fs.rename(oldpath, newpath, (err) => {
+          if (err){
+            res.status(400).jsonp({ error: 'internal server error' });
+          }else{
+            res.send('ok');
+          }
+        });
+      }
+    });
+  });
 
   return router;
 };
