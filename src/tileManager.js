e8286e7887e8906bf1a679dfb4fe40a15c61d551
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-10-17 19:48:06 -0700

    add ability to inject script -- this is not a stream, so maybe improve in the future if files are larger

--- src/tileManager.js
+++ src/tileManager.js
@@ -28,9 +28,70 @@ const deleteTile = tileName => new Promise((resolve, reject) => {
 });
 
 
+const scripts = {
+  common:  `<!--automate:inject--><script src="../common.js"></script><!--automate:inject-->\n`,
+  env: `<!--automate:inject--><script src="../env.js"></script><!--automate:inject-->\n`,
+};
+
+const injectScript =  (filePath, scriptName) => new Promise((resolve, reject) => {
+  const scriptContent = scripts[scriptName];
+  if (scriptContent === undefined) {
+    reject(`script: ${scriptName} is not defined`);
+  }
+  fs.readFile(filePath, (err, fileBuffer) =>  {
+    if (err){
+      reject(err);
+    }else{
+      const scriptInjectBuffer =  new Buffer(scriptContent);
+      const newFile = Buffer.concat([scriptInjectBuffer, fileBuffer]).toString();
+      fs.writeFile(filePath, newFile, err =>  {
+        if (err){
+          reject(err);
+        }else{
+          resolve();
+        }
+      });
+      resolve();
+    }
+  });
+});
+
+const removeScript =  (filePath, scriptName) =>  new Promise((resolve, reject) => {
+  const scriptContent = scripts[scriptName];
+  if (scriptContent === undefined) {
+    reject(`script: ${scriptName} is not defined`);
+  }
+  fs.readFile(filePath, (err, fileBuffer) =>  {
+    if (err){
+      reject(err);
+    }else {
+      const fileString = fileBuffer;
+
+      const index = fileBuffer.indexOf(scriptContent);
+      const lowBuffer = fileString.slice(0, index);
+      const highBuffer = fileString.slice(index + scriptContent.length);
+
+      if (index >= 0) {
+        const newBuffer = Buffer.concat([lowBuffer, highBuffer]).toString();
+        fs.writeFile(filePath, newBuffer, err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        reject('file does not contain injected script');
+      }
+      resolve();
+    }
+  });
+});
 
 module.exports = {
   getTiles,
   getTileDirectory,
   deleteTile,
+  injectScript,
+  removeScript,
 };
