553a30e1b6c628be3cd047c3da74b3454029dba4
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-10-19 15:08:56 -0700

    hooked up backedn to lock system, used flat json file

--- src/lockSystemManager.js
+++ src/lockSystemManager.js
@@ -1,19 +1,60 @@
 
+const path = require('path');
+const fs = require('fs');
+
 let isLocked = false;
 let systemName = undefined;
 
-const lockSystem = sysName => new Promise((resolve, reject) => {
-  isLocked = true;
-  systemName = sysName;
-  resolve();
-});
+const lockFileName = path.resolve(__dirname,'../data/lock.json');
+
+// checks if lock.json is there at system start
+const verifySystemResources = () => {
+  console.log('checking');
+  const exists = fs.existsSync(lockFileName);
+  if (!exists) {
+    console.log('warning: lock.json does not exist');
+  }
+};
+
+verifySystemResources();
 
 const getSystemLockedData = () => new Promise((resolve, reject) => {
-  resolve({
-    isLocked,
-    systemName,
-  });
+  fs.readFile(lockFileName, (err, data) => {
+    if (err){
+      reject(err);
+    }else{
+      try{
+        const info = JSON.parse(data.toString());
+        resolve(info);
+      }catch(err){
+        reject(err);
+      }
+    }
+  })
 });
+
+const lockSystem = systemName => {
+  if (systemName !== undefined && typeof(systemName) !== 'string'){
+    throw (new Error('lockSystemManager: systenName should either be unspecified or a string'));
+  }
+
+  const jsonToWrite = {
+    isLocked: true,
+  };
+  if (systemName){
+    jsonToWrite['systemName'] = systemName;
+  }
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(lockFileName,  JSON.stringify(jsonToWrite), err => {
+      if (err){
+        reject(err);
+      }else{
+        resolve();
+      }
+    })
+  });
+};
 
 module.exports = {
   lockSystem,
