63b315e246925263d5bc54613a24853cedea96b1
brad medeiros <bradmedeiros0@gmail.com>
2019-01-10 21:42:21 -0800

    delete dead code for actions scripts, will be resurrected explicitly for action scripts

--- src/communication/http/routes/system/actions.js
+++ src/communication/http/routes/system/actions.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const path = require('path');
-
-const createActionScriptContent = (system, name, topic, script, toTopic) => {
-  const actionScript = system.engines.actionScriptEngine.getActionScripts()[name];
-  if (actionScript === undefined){
-    return system.engines.actionScriptEngine.addActionScript(name, topic, script, toTopic);
-  }else{
-    console.log('tur');
-    return new Promise((resolve, reject) => {
-      system.engines.actionScriptEngine.deleteActionScript(name).then(() => {
-        system.engines.actionScriptEngine.addActionScript(name, topic, script, toTopic).then(resolve).catch(reject);
-      }).catch(reject);
-    });
-  }
-};
 
 const create_routes = system => {
   if (system === undefined){
-    throw (new Error('http:create_routes:states system must be defined'));
+    throw (new Error('http:create_routes:actions system must be defined'));
   }
-
 
   const router = express();
   router.get('/', (req, res) => {
@@ -30,51 +14,6 @@ const create_routes = system => {
         value: systemActions[actionName].value,
       }))
     });
-  });
-
-  router.post('/modify/*', (req, res) => {
-    if (req.body === undefined){
-      res.status(400).jsonp({ error: 'invalid parameters' });
-      return;
-    }
-
-    const name = path.relative('/modify', req.url);
-    const actionEval = req.body.actionEval;
-
-    if (system.baseSystem.actions.getActions()[name] === undefined){
-      system.baseSystem.actions.forceAddAction(path.relative('actions', name)).then(() => {
-        createActionScriptContent(system, name, name, actionEval || '', `action_scripts/${name}`).then(() => {
-          res.status(200).send('ok');
-        }).catch(() => {
-          res.status(500).jsonp({ error: 'internal server error' })
-        })
-      }).catch(() => {
-        res.status(500).jsonp({ error: 'internal server error' })
-      });
-    }else{
-      createActionScriptContent(system, name, name, actionEval || '', `action_scripts/${name}`).then(() => {
-        res.status(200).send('ok');
-      }).catch(() => {
-        res.status(500).jsonp({ error: 'internal server error' })
-      })
-    }
-  });
-
-  router.delete('/*', (req, res) => {
-    const name = path.relative('/', req.url);
-
-    if (system.engines.actionScriptEngine.getActionScripts()[name]){
-      system.engines.actionScriptEngine.deleteActionScript(name).then(() => {
-        system.baseSystem.actions.unregister(name).then(() => {
-          res.status(200).send('ok');
-        }).catch(() => {
-          res.status(500).jsonp({ error: 'internal server error' });
-        });
-      }).catch(() => res.status(500).jsonp({ error: 'internal server error' }));
-    }else{
-      system.baseSystem.actions.unregister(name);
-      res.status(200).send('ok');
-    }
   });
 
   return router;
