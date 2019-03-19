f3a26e8c88203869c76db6315bf3db7c38ea3743
brad medeiros <bradmedeiros0@gmail.com>
2019-03-18 23:17:10 -0700

    remove schedule, rules, conditions

--- src/communication/http/rest.js
+++ src/communication/http/rest.js
@@ -6,13 +6,9 @@ const create_account_routes = require('./routes/accounts');
 
 const create_state_routes = require('./routes/system/states');
 const create_action_routes = require('./routes/system/actions');
-const create_condition_routes = require('./routes/system/conditions');
 
 const create_actionscripts = require('./routes/system/actionscripts');
 const create_sequences_routes =  require('./routes/system/sequences');
-const create_schedule_routes = require('./routes/system/schedules');
-const create_rules_routes = require('./routes/system/rules');
-
 const create_event_routes = require('./routes/events');
 
 const create_database_routes = require('./routes/databases');
@@ -74,11 +70,8 @@ const create_routes = ({
   router.get('/', (req, res) => res.sendFile(path.resolve('./public/index.html')));
   router.use('/states', create_state_routes(system));
   router.use('/actions', create_action_routes(system));
-  router.use('/conditions', create_condition_routes(system));
   router.use('/actionscripts', create_actionscripts(system));
   router.use('/sequences', create_sequences_routes(system));
-  router.use('/schedules', create_schedule_routes(system));
-  router.use('/rules', create_rules_routes(system));
   router.use('/events', create_event_routes(system));
   router.use('/env', create_env(system));
   router.use('/databases', create_database_routes(databaseManager));
